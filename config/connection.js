7f3ff2ef916123b7ab439546fde0c5ea8d75c0cb
trishamasbate <trisha.masbate@gmail.com>
2024-07-23 16:07:29 -0600

    Modified the connection.js file to accommodate Render.

--- config/connection.js
+++ config/connection.js
@@ -1,8 +1,16 @@
 const Sequelize = require('sequelize');
 require('dotenv').config(); // Import sensitive data from .env
 
-const sequelize = process.env.JAWSDB_URL 
-  ? new Sequelize(process.env.JAWSDB_URL) // JAWSDB_URL is for HEROKU
+const sequelize = process.env.DATABASE_URL 
+  ? new Sequelize(process.env.DATABASE_URL, {
+      dialect: 'postgres',
+      dialectOptions: {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false, // This is required for Render
+        },
+      },
+    })
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, { // This is for Local
       host: 'localhost',
       dialect: 'postgres',
