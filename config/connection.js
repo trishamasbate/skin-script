c3b10f9c1f87bb2111ce97e8aa89a969428501f2
trishamasbate <trisha.masbate@gmail.com>
2024-07-23 16:25:11 -0600

    Debugging the .env file code.

--- config/connection.js
+++ config/connection.js
@@ -1,16 +1,8 @@
 const Sequelize = require('sequelize');
 require('dotenv').config(); // Import sensitive data from .env
 
-const sequelize = process.env.DATABASE_URL 
-  ? new Sequelize(process.env.DATABASE_URL, {
-      dialect: 'postgres',
-      dialectOptions: {
-        ssl: {
-          require: true,
-          rejectUnauthorized: false, // This is required for Render
-        },
-      },
-    })
+const sequelize = process.env.JAWSDB_URL 
+  ? new Sequelize(process.env.JAWSDB_URL) // JAWSDB_URL is for HEROKU
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, { // This is for Local
       host: 'localhost',
       dialect: 'postgres',
