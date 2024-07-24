7708ec6cb6a306c84677b503b12f941ab62af3cc
trishamasbate <trisha.masbate@gmail.com>
2024-07-23 20:22:52 -0600

    Connect to Postgresql.

--- config/connection.js
+++ config/connection.js
@@ -1,22 +1,20 @@
 const Sequelize = require('sequelize');
 require('dotenv').config(); // Import sensitive data from .env
 
-const sequelize = process.env.DATABASE_URL 
-  ? new Sequelize(process.env.DATABASE_URL, {
-      dialect: 'postgres',
-      dialectOptions: {
-        ssl: {
-          require: true,
-          rejectUnauthorized: false, // This is required for Render
-        },
-      },
-    })
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, { // This is for Local
+let sequelize;
+
+if (process.env.DB_URL) {
+  sequelize = new Sequelize(process.env.DB_URL);
+} else {
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PW,
+    {
       host: 'localhost',
       dialect: 'postgres',
-      dialectOptions: {
-        decimalNumbers: true,
-      },
-    });
+    },
+  );
+}
 
 module.exports = sequelize;
